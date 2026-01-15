c8910ee69554d5542e7f52d1812247a3cdac6f8d
Dmitry Kiselev <83563276+DmitryK579@users.noreply.github.com>
2026-01-15 00:30:01 +0000

    Ship prefab and controllers
    Ship and its turrets became prefabs. Made controllers for ship and turrets abstract so AI can control them in the future. Implemented turret rotation and aim logic.

---
+++ Assets/Scripts/ShipHandler.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class ShipHandler : MonoBehaviour
+{
+	[Header("Scripts")]
+	[SerializeField] private ShipController shipController;
+	[Header("Ship settings")]
+	[SerializeField] private float accelerationFactor = 1.0f;
+	[SerializeField] private float rotationFactor = 1.0f;
+	[SerializeField] private float driftFactor = 1.0f;
+
+	private Rigidbody2D shipRigidbody2D;
+
+	private void Awake()
+	{
+		shipRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void FixedUpdate()
+	{
+		ApplyEngineForce();
+		ApplyShipRotation();
+		HandleDrift();
+	}
+	void ApplyEngineForce()
+	{
+		Vector2 engineForceVector = transform.up * shipController.GetMovementInput().y * accelerationFactor;
+		shipRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
+	}
+
+	void ApplyShipRotation()
+	{
+		var torque = -shipController.GetMovementInput().x * rotationFactor;
+		shipRigidbody2D.AddTorque(torque, ForceMode2D.Force);
+	}
+
+	void HandleDrift()
+	{
+		//Kill orthogonal velocity so ship doesn't drift as if in space
+		Vector2 forwardVelocity = transform.up * Vector2.Dot(shipRigidbody2D.linearVelocity, transform.up);
+		Vector2 rightVelocity = transform.right * Vector2.Dot(shipRigidbody2D.linearVelocity, transform.right);
+
+		shipRigidbody2D.linearVelocity = forwardVelocity + rightVelocity * driftFactor;
+	}
+}
